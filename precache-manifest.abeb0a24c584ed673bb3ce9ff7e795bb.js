ead195440535f0975e0c599beb11fc90df4f26de
Hamza <hamzaable@gmail.com>
2021-06-05 01:43:23 +0200

    Updates

---
+++ precache-manifest.abeb0a24c584ed673bb3ce9ff7e795bb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "658905dda84cbcadccd9dbb273673cd7",
+    "url": "/Job-Listing/index.html"
+  },
+  {
+    "revision": "ac841317e0f6349dea6c",
+    "url": "/Job-Listing/static/css/main.a1c12547.chunk.css"
+  },
+  {
+    "revision": "55fdc554c5fb6b3003db",
+    "url": "/Job-Listing/static/js/2.eb22f1da.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Job-Listing/static/js/2.eb22f1da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac841317e0f6349dea6c",
+    "url": "/Job-Listing/static/js/main.23d3e065.chunk.js"
+  },
+  {
+    "revision": "e1be67b3ddccbad10d5c",
+    "url": "/Job-Listing/static/js/runtime-main.2a6e3fee.js"
+  },
+  {
+    "revision": "6e2f7196c0644447b6887a48a265bdb1",
+    "url": "/Job-Listing/static/media/bg-header-desktop.6e2f7196.svg"
+  }
+]);
